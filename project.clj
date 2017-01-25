7afe4f4f053e86851df7aa31f0045c2486963c31
puredanger <alex.miller@cognitect.com>
2017-01-25 16:00:53 -0600

    slimmed it down some more

--- project.clj
+++ project.clj
@@ -4,11 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [org.clojure/core.async "0.2.395"]
-                 [org.clojure/test.check "0.9.0"]
 ;;                 [org.clojure/tools.analyzer.jvm "0.6.10"]
                  [org.clojure/tools.analyzer.jvm "0.6.11-SNAPSHOT"]
-                 [org.clojure/tools.reader "1.0.0-beta3"]]
+                 [org.clojure/core.async "0.2.395"]]
   :repositories [["clojars" {:url "https://clojars.org/repo/"}]
                  ["maven-central" {:url "https://repo1.maven.org/maven2"}]
                  ["sonatype-oss-public" {:url "https://oss.sonatype.org/content/groups/public/"}]])
--- src/tajvm121/core.clj
+++ src/tajvm121/core.clj
@@ -1,11 +1,6 @@
 (ns tajvm121.core
-  (:require [clojure.core.async :as async :refer [go go-loop >! <!]]
-            [clojure.java.io :as jio]
-            [clojure.spec :as s])
-  (:import [java.io File]
-           [java.nio.file Files FileSystems FileVisitResult Path
-                          SimpleFileVisitor StandardWatchEventKinds
-                          WatchKey WatchEvent WatchService]))
+  (:require [clojure.core.async :as async :refer [go go-loop >! <!]])
+  (:import [java.nio.file StandardWatchEventKinds]))
 
 (def ^:private event->kind
   {:create StandardWatchEventKinds/ENTRY_CREATE
